8e9d85496666d51504f82bf918f44043cc833656
med363 <blibechmedamine@gmail.com>
2023-06-04 17:53:30 +0100

    completet package b

--- src/main/java/com/example/bank_app/contact/ContactRepository.java
+++ src/main/java/com/example/bank_app/contact/ContactRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ContactRepository extends JpaRepository<Contact,Integer> {
+    /*find all contact of user*/
     List<Contact>findAllByUserId();
 }
